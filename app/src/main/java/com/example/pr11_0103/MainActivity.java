ed5c132ea11c8311a7340d6bc512df97a5e8c654
Pavel Osokin <pachkaosokin@yandex.ru>
2024-11-17 20:15:08 +0500

    прописана функция для обработки нажатий на кнопки. Осокин

--- app/src/main/java/com/example/pr11_0103/MainActivity.java
+++ app/src/main/java/com/example/pr11_0103/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.pr11_0103;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +23,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void AlertDialog(String title, String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    public void OnClick(View view){
+        AlertDialog("Уведомление", "Нажатие обработано!");
     }
 }
